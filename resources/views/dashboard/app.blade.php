854d1710458e5dc73f1dbca86e6afd3e818503e2
Trần Xuân Trường <tranxuantruong2001z@gmail.com>
2024-06-14 08:58:49 +0700

    edit Auth
    sửa return redirect()->route('login'); ->   return redirect()->route('home');

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -175,7 +175,6 @@
                                         </li>
                                         <li>
                                             <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="{{ url('profile') }}">My Profile</a>
                                             <a class="dropdown-item" href="{{ route('logout') }}"
                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                                 Logout
